9607feb1c43e8bfb1c0bfdec07990a8c3f035319
rimasultana <rimasultana20225@gmail.com>
2024-11-15 17:13:36 +0600

    project started authentication

---
+++ src/layouts/AuthLayOut.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const AuthLayOut = () => {
+    return (
+        <div>
+            AuthLayout
+            <Outlet/>
+        </div>
+    );
+};
+
+export default AuthLayOut;
